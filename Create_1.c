4feb26f8956b1334eead4bf925fd240f8effac89
SadieRS <128521492+SadieRS@users.noreply.github.com>
2023-04-20 08:43:33 -0400

    Update Create_1.c

--- Create_1.c
+++ Create_1.c
@@ -1,38 +1,51 @@
-// claw positions
-Int clawopen= ;
-Int clawredring= ;
-Int claworgring= ;
-Int clawyelring= ;
-Int clawgrering= ;
-Int clawbluring= ;
+// claw positions (servo value positions)
+int clawopen= ;
+int clawredring= ;
+int claworaring= ;
+int clawyelring= ;
+int clawgrering= ;
+int clawbluring= ;
 
 //arm movement
-Int liftspeed= ; //positive value moves the arm from the ground, up and towards the tower. Negative value moves the arm away from the tower and down towards the ground
-Int armtall= ; //moves claw from cube drop position to the top of the tall tower
-Int armshort= ; //moves claw from cube drop position to the top of the short tower
-Int dropcubet= ; //moves cube from top of tall tower to the location to drop it
-Itn dropcubes= ; //moves cube from the top of short tower to the location to drop it
-Int armredring= ; //moves arm from dropping cube to grabbing red ring from rock-a-stack
-Int armorgring= ; //moves arm from dropping cube to grabbing orange ring from rock-a-stack
-Int armyelring= ; //moves arm from dropping cube to grabbing yellow ring from rock-a-stack
-Int armgrering= ; //moves arm from dropping cube to grabbinggreen  ring from rock-a-stack
-Int armbluring= ; //moves arm from dropping cube to grabbing blue ring from rock-a-stack
+int liftspeed= ; //positive value moves the arm from the ground, up and towards the tower. Negative value moves the arm away from the tower and down towards the ground
+int armtall= ; //moves claw from cube drop position to the top of the tall tower
+int armshort= ; //moves claw from cube drop position to the top of the short tower
+int dropcubet= ; //moves cube from top of tall tower to the location to drop it
+itn dropcubes= ; //moves cube from the top of short tower to the location to drop it
+int armredring= ; //moves arm from dropping cube to grabbing red ring from rock-a-stack
+int armoraring= ; //moves arm from dropping cube to grabbing orange ring from rock-a-stack
+int armyelring= ; //moves arm from dropping cube to grabbing yellow ring from rock-a-stack
+int armgrering= ; //moves arm from dropping cube to grabbinggreen  ring from rock-a-stack
+int armbluring= ; //moves arm from dropping cube to grabbing blue ring from rock-a-stack
+int tostand= ; //moves arm from cube drop position to ground to grab ring stand 
 
 // ports
-Int claw= ;
-Int arm1= ;
-Int arm2= ;
+int claw= ;
+int arm1= ;
+int arm2= ;
 
 //general movement
-Int speed= ; //speed for driving
-Int cm;  //ticks straight cm convertion
+int speed= ; //speed for driving
+int cm;  //ticks straight cm convertion
+int turnspeed= ; //speed for turning. Use positive and negative
 
-
-Void ringstand { 		//closes claw to grab ring stand for transportation
-Set_servo_position(claw,clawclose);
+//functions
+void grabstand {
+  set_servo_position(claw,clawclose);
 }
 
-Void tallcube { 		//moves claw from down on the ground to the top tall tower, grabs cube and moves to position to drop cube, drops cube
+void ringstand { 		//moves arm from cube drop to ground and closes claw to grab ring stand for transportation
+cmpc(arm1)
+while(gmpc(arm1)>tostand) {
+  mav(arm1,-liftspeed);
+  mav(arm2,-liftspeed);
+}
+  freeze(arm1);
+  freeze(arm2);
+  Set_servo_position(claw,clawclose);
+}
+
+void tallcube { 		//moves claw from down on the ground to the top tall tower, grabs cube and moves to position to drop cube, drops cube
 set_servo_position(claw,clawopen);
 cmpc(arm1);
 while (gmpc(arm1)<armtall) {
@@ -53,7 +66,7 @@ set_servo_position(claw,clawopen);
 cmpc(arm1);
 }
 
-Void shortcube {			//moves claw from down on ground to short tower, grabs cube, moves to position to drop, drops cube
+void shortcube {			//moves claw from down on ground to short tower, grabs cube, moves to position to drop, drops cube
 set_servo_position(claw,clawopen);
 cmpc(arm1);
 while (gmpc(arm1)<armshort) {
@@ -74,7 +87,7 @@ set_servo_position(claw,clawopen);
 cmpc(arm1);
 }
 
-Void redring {			//moves arm from cube drop position to rock-a-stack, grabs red ring, lifts ring to short tower, releases ring, returns to cube drop position.
+void redring {			//moves arm from cube drop position to rock-a-stack, grabs red ring, lifts ring to short tower, releases ring, returns to cube drop position.
 while(gmpc(arm1)>-armredring) {
 mav(arm1,-liftspeed);
 mav(arm2,-liftspeed);
@@ -102,32 +115,107 @@ mav(arm2,-liftspeed);
 freeze(arm1);
 freeze(arm2);
 }
+  
+void orangering {			//moves arm from cube drop position to rock-a-stack, grabs orange ring, lifts ring to tall tower, releases ring, returns to cube drop position.
+while(gmpc(arm1)>-armoraring) {
+mav(arm1,-liftspeed);
+mav(arm2,-liftspeed);
+}
+freeze(arm1);
+freeze(arm2);
+set_servo_position(claw,claworaring);
+cmpc(arm1);
+while(gmpc(arm1)<-armoraring) {
+mav(arm1,liftspeed);
+mav(arm2,liftspeed);
+cmpc(arm1);
+While(gmpc(arm1)<armtall) {
+mav(arm1,liftspeed);
+mav(arm2,liftspeed);
+}
+freeze(arm1);
+freeze(arm2);
+set_servo_position(claw,clawopen);
+cmpc(arm1);
+while(gmpc(arm1)>-armtall) {
+mav(arm1,-liftspeed);
+mav(arm2,-liftspeed);
+}
+freeze(arm1);
+freeze(arm2);
+}
 
-Void wallride {
+void wallride {
 create_drive_direct(speed, (speed+10));
 }
 
-Void driveforward(int distance) {
+void driveforward(int distance) {
 set_create_distance(0);
 while(get_create_distance()< (distance*cm)) {
 create_drive_direct(speed,speed);
 }
 create_stop();
 
-Void rturn(int angle) {
+void rturn(int angle) {
 set_create_total_angle(0);
 while(get_create_total_angle()<angle*degrees)) {
 create_drive_direct(turnspeed,-turnspeed);
 }
 create_stop();
+}
 
-Void lturn(int angle) {
+void lturn(int angle) {
 set_create_total_angle(0);
 while(get_create_total_angle()<(angle*degrees)) {
 create_drive _direct(-turnspeed,turnspeed);
 }
 create_stop();
+}
 
-Int main {
-
-
+void toorange {
+  open_camera();
+  while(get_object_area_  {
+  wallride;
+  }
+        create_stop();
+        }
+        
+void toyellow {
+  open_camera();
+  while(get_object_area  {
+    wallride;
+  }
+        create_stop();
+        }
+  
+void togreen {
+  open_camera();
+  while(get_object_area   {
+  wallride;
+  }
+        create_stop();
+        }
+void toblue {
+  open_camera();
+  while(get_object_area   {
+    create_drive_direct(-speed,-speed);
+  }
+        create_stop();
+        }
+  
+  
+//main
+int main {
+  //leave start box, prep for cubes and rings
+  grabstand; //arm starts down, claw closes around ring stand for transportation
+  driveforward(_);
+  rturn(90)
+  create_stop();
+  shortcube;
+  redring;
+  ringstand;
+  toorange;
+  org
+  
+  
+  
