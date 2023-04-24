309505647acccfbb842e78a650150f561a132f16
SadieRS <128521492+SadieRS@users.noreply.github.com>
2023-04-24 14:44:20 -0400

    Update Create3.0.c

--- Create3.0.c
+++ Create3.0.c
@@ -2,17 +2,19 @@
 #include <kipr/wombat.h>
 
 //Variables
+int arm=0; //port for arm motor
+int claw=2; //port for claw servo
 
-int lspeed=300;
-int rspeed=300;
+int lspeed=185;
+int rspeed=185;
 
-int lwall=280;
-int rwall=240;
+int lwall=265;
+int rwall=220;
 
-int rTspeed=200;
-int lTspeed=200;
+int rTspeed=150;
+int lTspeed=150;
 
-float deg=1.65;
+float deg=1.12;
 int CM=10;
 
 int towerDis;
@@ -21,18 +23,21 @@ int motor0 = 0;
 int motor1 = 1;
 int armSpeed = 275;
 
-int open ;
-int r1 ; //open value for given ring
+int open = 900 ;
+int r1 ; //open claw value for given ring
 int r2 ;
 int r3 ;
 int r4 ;
 int r5 ;
-int r1c ; //closed on given ring
+int r1c = 38 ; //closed claw on given ring
 int r2c ;
 int r3c ;
 int r4c ;
 int r5c ;
-int cube ; //closnig on a cube
+int cube= 60; //closed claw on a cube
+
+int high = 920;
+int low = 1150;
 
 //Functions
 
@@ -60,24 +65,16 @@ void armDown(int position);
 int main()
 {
   create_connect();
-  
-  lturn(90);
-  
-  while(get_create_lbump()==0) {
-    create_drive_direct(lspeed, rspeed);
-  }
-  create_stop();
-  
-  back(3);
-  rturn(90);
-  
-  wallRide(150);
-  
-  msleep(500);
-  
-  wallRideBack(100);
-  
-  msleep(1000);
+  cmpc(motor0);
+    set_servo_position(claw,open);
+    enable_servos(claw);
+  armUp(low);
+    msleep(1000);
+    set_servo_position(claw,cube);
+    msleep(1000);
+    armDown(1000);
+    msleep(1000);
+    
   
   create_disconnect();
 }
@@ -148,11 +145,11 @@ void wallRideBack(int distance) {
   
   while (abs(get_create_distance()) < (distance*CM)) {
     while (get_create_lbump()==0) {
-      create_drive_direct(0,400);
+      create_drive_direct(0,200);
     }
-    rturn(25);
-    create_drive_direct(-350,-350);
-    msleep(75);
+    rturn(3);
+    create_drive_direct(-350,-400);
+    msleep(350);
   }
   create_stop();
 }
@@ -204,27 +201,24 @@ void offTowerBack() {
 
 
 void armUp (int position) {
-  while(gmpc(0) < position) {
+  while(gmpc(arm) < position) {
     mav(motor0, armSpeed);
     mav(motor1, armSpeed);
   }
+  freeze(motor0);
+  freeze(motor1);
+  msleep(100);
   ao();
 }
 
 
 void armDown (int position) {
-  while(gmpc(0) > position) {
+  while(gmpc(arm) < position) {
     mav(motor0, -armSpeed);
     mav(motor1, -armSpeed);
   }
+  freeze(motor0);
+  freeze(motor1);
+  msleep(100);
   ao();
 }
-
-
-
-
-
-
-
-
-
