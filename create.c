5e717ae9459b16e28a48f6045e7de847ebfca7af
Bob-Ross07 <128514351+Bob-Ross07@users.noreply.github.com>
2023-04-28 08:22:06 -0400

    Update create.c

--- create.c
+++ create.c
@@ -14,18 +14,43 @@ int line = 1500;
 
 void forward(int distance) {
     set_create_distance(0);
-    while (get_create_distance() < distance) {
+    while (get_create_distance() < distance*10) {
         mav(rspeed, lspeed);
         msleep(10);
     }
+    create_drive_direct(0,0);
+    msleep(10);
 }
     
 void backward(int distance) {
     set_create_distance(0);
-    while (abs(get_create_distance()) < distance) {
+    while (abs(get_create_distance()) < distance*10) {
         mav(-rspeed, -lspeed);
         msleep(10);
     }
+    create_drive_direct(0,0);
+    msleep(10);
+}
+
+void wallride(int distance) {
+    set_create_distance(0);
+    while(get_create_distance() < distance*10) {
+        if((get_create_rbump()==0) && (get_create_lbump()==0)) {
+            create_drive_direct(rspeed, lspeed);
+            msleep(10);
+        }
+        if((get_create_rbump()==1) && (get_create_lbump()==0)) {
+            create_drive_direct(rspeed, lspeed-100);
+            msleep(10);
+        }
+        if((get_create_rbump()==0) && (get_create_lbump()==1)) {
+            create_drive_direct(rspeed-100, lspeed);
+            msleep(10);
+        }
+        
+    }
+    create_drive_direct(0,0);
+    msleep(10);
 }
     
 void rturn(int angle) {
@@ -33,12 +58,16 @@ void rturn(int angle) {
         mav(-rspeed, lspeed);
         msleep(10);
     }
+    create_drive_direct(0,0);
+    msleep(10);
 }
 void lturn(int angle) {
     while (get_create_total_angle() < angle) {
         mav(rspeed, -lspeed);
         msleep(10);
     }
+    create_drive_direct(0,0);
+    msleep(10);
 }
 
 void high_grab() {
@@ -52,9 +81,18 @@ void high_grab() {
     }
     freeze(arm1);
     freeze(arm2);
-    forward(50);
+    forward(5);
     set_servo_position(claw, close);
     msleep(150);
+    backward(10);
+    lturn(90);
+    while(digital(low) == 0) {
+        mav(arm1, downspeed);
+        mav(arm2, downspeed);
+        msleep(10);
+    }
+    set_servo_position(claw, open);
+    msleep(100);   
 }
 
 void low_grab() {
@@ -78,22 +116,39 @@ void align() {
         mav(rspeed, lspeed);
         msleep(10);
     }
-    
+    create_drive_direct(0, 0);
+    msleep(10);
     while(get_create_rfcliff_amt() <= line) {
-      mav(0, lspeed);
+      mav(0, -lspeed);
       msleep(10);
     }
 }
     
 int main() {
     create_connect();
+    //drive over to towers
     align();
-    forward(300);
+    while(get_create_lbump() == 0) {
+        forward(1);
+        msleep(10);
+    }
     msleep(500);
-    backward(300);
+    backward(5);
+    rturn(90);
+    wallride(70);
+    
+    //grab botgal
+    lturn(90);
+    backward(5);
+    rturn(25);
+    align();
+    backward(5);
     high_grab();
-    msleep(500);
-    low_grab();
+    
+    //grab cubes
+    
+    
+    
     create_disconnect();
     return 0;
 }
